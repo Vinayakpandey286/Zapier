227bd2c09b457ecd5e22b974186cbfe0b4a95c8e
Vinayak pandey <Vinayakpandey286@gmail.com>
2024-10-18 13:52:23 +0530

    added actions and avaialbe actions

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -1,6 +1,18 @@
 import express from 'express'
+import {} from '@prisma/client'
 
 const app = express()
 
 // hooks endpoint in zapier is like this 
 // htttps://hooks.zapier.com/hooks/catch/170346863/4276737397
+
+app.post('/hooks/catch/:userId/:zapId', async (req, res) => {
+    
+    const zapId = req.params.zapId
+    const userId = req.params.userId
+    const body = req.body
+
+
+})
+
+app.listen(3000)
