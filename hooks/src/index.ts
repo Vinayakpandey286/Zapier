8d117332769fafb75e7e3f558095f394bf90ef49
Vinayak pandey <Vinayakpandey286@gmail.com>
2024-10-18 15:18:52 +0530

    added transaction ts bug fixed

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -11,12 +11,30 @@ app.use(express.json())
 // htttps://hooks.zapier.com/hooks/catch/170346863/4276737397
 
 app.post('/hooks/catch/:userId/:zapId', async (req, res) => {
-    
+
     const zapId = req.params.zapId
     const userId = req.params.userId
     const body = req.body
 
+    await client.$transaction(async tx => {
+        const run = await tx.zapRun.create({
+            data: {
+                zapId, 
+                metadata:body
+                
+            }
+        })
+
+        await tx.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id
+            }
+        })
+    })
+
 
 })
 
-app.listen(3000)
+app.listen(3000, ()=>{
+    console.log('app is listening')
+})
